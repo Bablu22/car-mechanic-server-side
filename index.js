d148436785d89991efbe396b54ab0f06e71d593b
bablu22 <mdbablu22.bablu22.com>
2021-11-16 12:21:10 +0600

    add services

--- index.js
+++ index.js
@@ -2,14 +2,21 @@ const express = require('express')
 const { MongoClient } = require('mongodb');
 const cors = require('cors')
 const ObjectId = require('mongodb').ObjectId
-
 require('dotenv').config()
+const stripe = require("stripe")(process.env.STRIPE_SECRET);
+const fileUpload = require('express-fileupload');
+
+
 const app = express()
 const port = process.env.PORT || 5000
 
 // MIddleware
+app.use(express.static("public"));
 app.use(cors())
 app.use(express.json())
+app.use(fileUpload())
+
+
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sqmxk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -23,12 +30,23 @@ async function run() {
 
         // POST API
         app.post('/services', async (req, res) => {
-
-            const service = req.body;
+            const name = req.body.name
+            const price = req.body.price
+            const pic = req.files.img
+            const picData = pic.data
+            const encodedData = picData.toString('base64')
+            const image = Buffer.from(encodedData, 'base64')
+            const service = {
+                name,
+                price,
+                img: image
+            }
             const result = await servicesCollection.insertOne(service);
             // console.log(`A document was inserted with the _id: ${result.insertedId}`);
             res.json(result)
         })
+
+
 
         // GET API
         app.get('/services', async (req, res) => {
@@ -44,7 +62,17 @@ async function run() {
             const service = await servicesCollection.findOne(query);
             res.json(service)
         })
-
+        app.put('services/:id', async (req, res) => {
+            const id = req.params.id
+            const filter = { _id: ObjectId(id) }
+            const updateDoc = {
+                $set: {
+                    payment: payment
+                }
+            }
+            const result = await servicesCollection.updateOne(filter, updateDoc)
+            res.json(result)
+        })
         // DELETE APi
         app.delete('/services/:id', async (req, res) => {
             const id = req.params.id
@@ -52,6 +80,32 @@ async function run() {
             const deleteService = await servicesCollection.deleteOne(query);
             res.json(deleteService)
         })
+
+        app.post("/create-payment-intent", async (req, res) => {
+            const paymentInfo = req.body;
+            const amount = paymentInfo.price * 100;
+
+            // Create a PaymentIntent with the order amount and currency
+            const paymentIntent = await stripe.paymentIntents.create({
+                currency: "eur",
+                amount: amount,
+                payment_method_types: [
+                    "giropay",
+                    "eps",
+                    "p24",
+                    "sofort",
+                    "sepa_debit",
+                    "card",
+                    "bancontact",
+                    "ideal",
+                ],
+            });
+
+            res.send({
+                clientSecret: paymentIntent.client_secret,
+            });
+        });
+
 
     }
     finally {
